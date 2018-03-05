fc35884f1479fa362c1af4d48bf369793c1ade72
Husham <hushamhassanto@gmail.com>
2018-03-05 03:41:08 -0500

    Added printing to debug, works now on my computer.

--- server/tune_scout_server/routes/intellitune.js
+++ server/tune_scout_server/routes/intellitune.js
@@ -11,13 +11,35 @@ const options = {
 const recognizer = new speechService(options);
 const ffmpeg = require('fluent-ffmpeg');
 
+const SECONDS = 1 / 1000;
+const MB = 1 / 1024 / 1024;
+
 /* GET lyrics. */
 router.get('/:id', function (req, res, next) {
+    console.log('Intellitune hit with id: ' + req.params.id);
     let result = '';
-    const stream = ytdl(req.params.id, { format: (format) => format.container === 'mp4' })
-        .pipe(fs.createWriteStream('video.mp4'));
+    const stream = ytdl(req.params.id, { format: (format) => format.container === 'mp4' });
+    let startTime = null;
+    let lastTime = null;
+    stream.on('progress', (chunkLength, downloaded, total) => {
+        if (!startTime) {
+            startTime = Date.now();
+            lastTime = startTime;
+        }
+        let currentTime = Date.now()
+        if ((currentTime - lastTime) * SECONDS > 1) {
+            lastTime = currentTime;
+            let downloadedSeconds = (currentTime - startTime) * SECONDS;
+            let floatDownloaded = downloaded / total;
+            console.log('\nDownloaded: ' + (floatDownloaded * 100).toFixed(2) + '%');
+            console.log('Elapsed Time: ' + downloadedSeconds.toFixed(2) + ' seconds');
+            console.log('Estimated time left: ' + (downloadedSeconds / floatDownloaded - downloadedSeconds).toFixed(2) + ' seconds');
+        }
+    });
+    stream.pipe(fs.createWriteStream('video.mp4'));
 
     stream.on('finish', () => {
+        console.log('Download completed!');
         ffmpeg('video.mp4')
             .output('video.wav')
             .run();
